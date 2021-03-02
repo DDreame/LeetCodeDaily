823851a73b413f36fcdb23fb3ec79ca672990fa8
x8140 <shadowxiaoruantian@gmail.com>
2021-03-02 23:22:25 +0800

    改周设定

--- src/main/java/March/README.md
+++ src/main/java/March/README.md
@@ -1,16 +1,23 @@
-#三月每日签到题板块
+# 三月每日签到题板块
 
-##总结
+## 总结
 
 
 
 
 ## 3月1日 区域和检索 - 数组不可变
 
-题目链接： https://leetcode-cn.com/problems/range-sum-query-immutable/  
+[题目链接](https://leetcode-cn.com/problems/range-sum-query-immutable/)  
 题目难度： Easy
 
-总结：Easy题确实是真的Easy题，我看到以后直接定义全局数组然后每次for循环计算从i到j就完了。事后看题解才知道是一个前缀和的裸题。
+总结：Easy题确实是真的Easy题，我看到以后直接定义全局数组然后每次for循环计算从i到j就完了。事后看题解才知道是一个前缀和的裸题。  
+有一个题解是从暴力到前缀和思路的递推，相当不错。
 
 ## 3月2日 二维区域和检索 - 矩阵不可变
 
+[题目链接](https://leetcode-cn.com/problems/range-sum-query-2d-immutable/submissions/)
+题目难度：Medium
+
+总结：是昨天题目的进阶版本，测试用例依然在可以暴力的范围之内，所以先用暴力提交了一遍。  
+使用昨天的前缀和题解进行自行思考，基本上达到了最优的解的情况，难点一是前缀和数组的制作，难点二是计算的时候重复扣掉的值需要加回来。除此外没有其他难点。  
+可以考虑使用面积覆盖来进行思索。属于Medium中的Easy题了。
--- src/main/java/March/Solution2.java
+++ src/main/java/March/Solution2.java
@@ -3,5 +3,55 @@ package March;
 public class Solution2 {
     /***
      *
-     */
+     *  暴力解题不具有技术含量结果非常差
+     *
+    int[][] nums;
+
+    public static void main(String[] args) {
+        int[][] nums = new int[][]{{3, 0, 1, 4, 2}, {5, 6, 3, 2, 1}, {1, 2, 0, 1, 5}, {4, 1, 0, 1, 7}, {1, 0, 3, 0, 5}};
+        System.out.println(sumRegion(2,1,4,3,nums));
+        System.out.println(sumRegion(1,1,2,2,nums));
+    }
+    public static int sumRegion(int row1, int col1, int row2, int col2,int[][] nums) {
+        int tag = 0;
+        int tmp = col1;
+        for(;row1 <=row2;row1++){
+            for(; col1 <=col2 ; col1++){
+                tag += nums[row1][col1];
+            }
+            col1 = tmp;
+        }
+        return tag;
+    }
+    **/
+
+    /**
+     *
+     *
+     * 算是标准题解了，根据昨天前缀和的题解思路进行思考的
+     * 难点是计算的时候到底如何确定下表，前缀和数组把握思路很容易思考，但是要返回确定值的时候需要考虑清楚减去的区域。
+     *
+     class NumMatrix {
+        int[][] nums;
+        public NumMatrix(int[][] matrix) {
+            if(matrix.length >0){
+                this.nums = new int[matrix.length+1][matrix[0].length+1];
+                for(int i = 1; i< nums.length; i++){
+                    for(int j = 1; j< nums[0].length; j++ ){
+                        nums[i][j] = nums[i][j-1] + nums[i-1][j] + matrix[i-1][j-1] - nums[i-1][j-1];
+                    }
+                }
+            }
+         }
+
+         public int sumRegion(int row1, int col1, int row2, int col2) {
+            return nums[row2+1][col2+1] - nums[row1][col2+1] - nums[row2+1][col1] + nums[row1][col1];
+         }
+         }
+     **/
+
+
+
+
+
 }
